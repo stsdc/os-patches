deea784fedefd74492f53843770dd8d154f08246
stsdc <6031763+stsdc@users.noreply.github.com>
2025-02-16 23:11:28 +0100

    .

--- get_latest_version.py
+++ get_latest_version.py
@@ -20,5 +20,22 @@ def get_packages_list() -> list:
         items = file.read().splitlines()
     return items
 
-pckgs = get_packages_list()
-print(pckgs)
+def main():
+    SERIES_NAME = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_SERIES_NAME
+
+    # Initialize APT
+    apt_pkg.init_system()
+
+    # Initialize Launchpad variables
+    launchpad = Launchpad.login_anonymously(
+        "elementary daily test", "production", "~/.launchpadlib/cache/", version="devel"
+    )
+
+    packages_and_upstream = get_packages_list()
+
+    for package_and_upstream in packages_and_upstream:
+        package, upstream_series = package_and_upstream.split(":", 1)
+        print(package, upstream_series)
+
+if __name__ == "__main__":
+    main()
